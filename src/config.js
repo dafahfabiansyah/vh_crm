23b767a4e569923cb2f25a40dc6b103f762f92a3
dafahfabiansyah <davarobiansyah@gmail.com>
2025-05-22 18:39:45 +0700

    Update HOST variable in config to use production URL and comment out local API_URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const config = {
   PORT: import.meta.env.VITE_PORT || "3000",
-  HOST: import.meta.env.VITE_HOST || "localhost",
-  API_URL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+  HOST: import.meta.env.VITE_HOST || "https://vh-crm.vercel.app/",
+//   API_URL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+  API_URL: import.meta.env.VITE_API_URL || "https://vh-crm.vercel.app/",
 };
